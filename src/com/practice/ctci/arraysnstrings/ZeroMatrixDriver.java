6c6fb3b0a39831ff195481362f92832b164f9d1c
Coder <coder@foo.com>
2022-09-18 19:21:17 +0530

    Vel - Zero Matrix - solved

---
+++ src/com/practice/ctci/arraysnstrings/ZeroMatrixDriver.java
@@ -0,0 +1,20 @@
+package com.practice.ctci.arraysnstrings;
+
+/**
+ * @author Vel
+ * @date 18/09/22 : 19:10
+ * Question link : https://leetcode.com/problems/set-matrix-zeroes/
+ * Question name : Set Matrix zeroes
+ * Question desc : Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+ * You must do it in place.
+ */
+public class ZeroMatrixDriver {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ZeroMatrixTools {
+    
+}
+
